c3204e8508d14779bf0f18fcb03afdd33e1dacf4
Raphael Magee <raphael.magee@gmail.com>
2000-01-01 00:05:03 +0000

    add quad motor control and update PWM values and links

--- include/libpwm.h
+++ include/libpwm.h
@@ -3,7 +3,7 @@
  */
 
 #define PWM_DUTY_FILE1			"/sys/devices/ocp.3/pwm_test_P8_13.18/duty"
-#define PWM_DUTY_FILE2			"/sys/devices/ocp.3/pwm_test_P8_19.16/duty"
-#define PWM_DUTY_FILE3			"/sys/devices/ocp.3/pwm_test_P9_14.15/duty"
+#define PWM_DUTY_FILE3			"/sys/devices/ocp.3/pwm_test_P8_19.16/duty"
+#define PWM_DUTY_FILE2			"/sys/devices/ocp.3/pwm_test_P9_14.15/duty"
 #define PWM_DUTY_FILE4			"/sys/devices/ocp.3/pwm_test_P9_16.17/duty"
-#define PWM_SPEED			1160000
+#define PWM_SPEED			1000000
