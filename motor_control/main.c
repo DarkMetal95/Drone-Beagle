f4a6d8cc1f6de4b0fb4ae0d8099040dd18be9d08
Paul-Emmanuel Raoul <skyper@skyplabs.net>
2015-02-24 10:56:45 +0100

    indent using UNIX style.

--- motor_control/main.c
+++ motor_control/main.c
@@ -7,33 +7,32 @@
 
 void changemode(int dir)
 {
-  static struct termios oldt, newt;
+	static struct termios oldt, newt;
 
-  if ( dir == 1 )
-  {
-    tcgetattr( STDIN_FILENO, &oldt);
-    newt = oldt;
-    newt.c_lflag &= ~( ICANON | ECHO );
-    tcsetattr( STDIN_FILENO, TCSANOW, &newt);
-  }
-  else
-    tcsetattr( STDIN_FILENO, TCSANOW, &oldt);
+	if (dir == 1)
+	{
+		tcgetattr(STDIN_FILENO, &oldt);
+		newt = oldt;
+		newt.c_lflag &= ~(ICANON | ECHO);
+		tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+	}
+	else
+		tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
 }
 
-int kbhit (void) 
+int kbhit(void)
 {
-  struct timeval tv;
-  fd_set rdfs;
+	struct timeval tv;
+	fd_set rdfs;
 
-  tv.tv_sec = 0;
-  tv.tv_usec = 0;
+	tv.tv_sec = 0;
+	tv.tv_usec = 0;
 
-  FD_ZERO(&rdfs);
-  FD_SET (STDIN_FILENO, &rdfs);
+	FD_ZERO(&rdfs);
+	FD_SET(STDIN_FILENO, &rdfs);
 
-  select(STDIN_FILENO+1, &rdfs, NULL, NULL, &tv);
-  return FD_ISSET(STDIN_FILENO, &rdfs);
-
+	select(STDIN_FILENO + 1, &rdfs, NULL, NULL, &tv);
+	return FD_ISSET(STDIN_FILENO, &rdfs);
 }
 
 int main()
@@ -43,36 +42,36 @@ int main()
 	char speed_c[] = "1160000";
 	long int speed = 1160000;
 	char key = 0x00;
-	FILE* fichier = NULL;
+	FILE *fichier = NULL;
 
 	fichier = fopen("/sys/devices/ocp.3/pwm_test_P8_13.15/duty", "w");
-	if(fichier == NULL)
+	if (fichier == NULL)
 	{
 		printf("Erreur d'ouverture du fichier\n");
 		return 0;
 	}
 
-	changemode(1);	
+	changemode(1);
 
 	fputs("1160000", fichier);
 
-	while(end)
+	while (end)
 	{
-		while(!kbhit());
+		while (!kbhit());
 		key = getchar();
-	
-		if(key == 'z')
+
+		if (key == 'z')
 		{
 			rewind(fichier);
 			speed += 1000;
-			sprintf(speed_c, "%d", speed);	
+			sprintf(speed_c, "%d", speed);
 			fputs(speed_c, fichier);
 		}
-		else if(key == 's')
+		else if (key == 's')
 		{
 			rewind(fichier);
 			speed -= 1000;
-			sprintf(speed_c, "%d", speed);	
+			sprintf(speed_c, "%d", speed);
 			fputs(speed_c, fichier);
 		}
 		else
